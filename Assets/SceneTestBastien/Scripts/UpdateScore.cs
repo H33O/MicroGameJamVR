7a27b4df2e2742a85e5cd4b059c2c21fe72ac636
BastienMedina <medina.bastien13@gmail.com>
2025-10-23 17:22:35 +0200

    [MBA] 23/10/2025 17:22 Music de fond dans prefab qui fonctionne

--- Assets/SceneTestBastien/Scripts/UpdateScore.cs
+++ Assets/SceneTestBastien/Scripts/UpdateScore.cs
@@ -13,6 +13,6 @@ public class UpdateScore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        textRef.text = "Score : " + scoreData.score.ToString();
+        textRef.text = scoreData.score.ToString();
     }
 }
