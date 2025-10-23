eee3b9698133f3ef5b236c9334f0cc05a0e188fc
H33O (Rivals_Hugo) <hugo.rivals@brassart.fr>
2025-10-23 15:56:32 +0200

    HRI test

--- Assets/SceneTestBastien/Scripts/Can.cs
+++ Assets/SceneTestBastien/Scripts/Can.cs
@@ -11,7 +11,7 @@ public class Can : MonoBehaviour
         if(collision.gameObject.GetComponent<Ground>() != null)
         {
             scoreData.score += 1;
-            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
