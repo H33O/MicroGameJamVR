0c7ddb185a864576bf584617e13a981352da461c
H33O (Rivals_Hugo) <hugo.rivals@brassart.fr>
2025-10-23 12:32:14 +0200

    J'ai pas le temps de travailler ptn

---
+++ Assets/Canva_Hand.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canva_Hand : MonoBehaviour
+{
+   
+    void Update()
+    {
+        GetComponent()
+    }
+}
